36927ed2fb1b524c8a34be0043c62b7cfc3df83c
Josh-Gotro <joshuagauthreaux@gmail.com>
2021-08-27 14:22:40 -0500

    add objects and arrays practice

---
+++ arrays.js
@@ -0,0 +1,51 @@
+
+// Array
+let portalLocations = ["Drellyn's Ferry", "Zathar House", "Osteroc", "Mines"];
+
+// use console.log to print the array to the console
+console.log(portalLocations);
+
+/* 1. -------------------------------------------------
+Practice adding and removing elements from an array using .unshift, .shift, .push, .pop */
+
+    //Add a location to the beginning of the array.
+
+
+    //Add a location to the end of the  array.
+
+
+    // Remove the first location from the array
+
+
+    // Remove the last location from the array
+
+
+
+
+
+/* 2. -------------------------------------------------
+Wrap statements in a function so you can call them at will. */
+
+    // Wrap one or more of the previous steps in a function and call the function. 
+
+
+
+
+
+
+/* 3. -------------------------------------------------
+Use the index of an element to find, add, and remove elements from the array
+Practice wrapping your statements in a function and calling them. */
+
+    // Find the index of "Mines"
+
+
+    // add "Hell" to portalLocations after "Zathar House" and before "Osteroc"
+
+
+    // Change "Hell" to "Ghost Lord's Keep"
+
+
+    // remove "Ghost Lord's Keep" from portalLocations
+
+
---
+++ objects.js
@@ -0,0 +1,44 @@
+// Object
+let playerCharacters = {
+    "Druid": "Akasha",
+    "Rogue": "Elektropop",
+    "Warlock": "Samuel",
+    "Monk": "Rabbit",
+    "Bard": "Nico",
+}
+console.log(playerCharacters)
+
+/* 1. Return data from an object */
+
+    // Return the value of "Warlock"
+
+    // BONUS : Return any key
+
+
+
+
+/* 2.  Add and remove key/value pairs */
+
+    // add the key/value pair, "Dragon Born" : "Za'a'a'a'a'thar"
+
+
+    // Remove key/value pair
+
+
+
+
+/* BONUS  */
+
+    // Refactor so the functions so they are reusable 
+
+
+
+
+
+/* 3. -------------------------------------------------
+Change value and key */
+
+    // Change value "Nico" to "Nicolas"
+
+
+    // Change key "Bard" to "BestClass"
