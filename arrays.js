f7639a75a12db7b0f4385744790e1caede813f2f
Josh-Gotro <joshuagauthreaux@gmail.com>
2021-08-29 09:21:57 -0500

    combine object worksheets

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 
-// Array
+// Array 1 
 let portalLocations = ["Drellyn's Ferry", "Zathar House", "Osteroc", "Mines"];
 
 // use console.log to print the array to the console
@@ -8,16 +8,16 @@ console.log(portalLocations);
 /* 1. -------------------------------------------------
 Practice adding and removing elements from an array using .unshift, .shift, .push, .pop */
 
-    //Add a location to the beginning of the array.
+//Add a location to the beginning of the array.
 
 
-    //Add a location to the end of the  array.
+//Add a location to the end of the  array.
 
 
-    // Remove the first location from the array
+// Remove the first location from the array
 
 
-    // Remove the last location from the array
+// Remove the last location from the array
 
 
 
@@ -26,7 +26,7 @@ Practice adding and removing elements from an array using .unshift, .shift, .pus
 /* 2. -------------------------------------------------
 Wrap statements in a function so you can call them at will. */
 
-    // Wrap one or more of the previous steps in a function and call the function. 
+// Wrap one or more of the previous steps in a function and call the function. 
 
 
 
@@ -37,15 +37,17 @@ Wrap statements in a function so you can call them at will. */
 Use the index of an element to find, add, and remove elements from the array
 Practice wrapping your statements in a function and calling them. */
 
-    // Find the index of "Mines"
+// Find the index of "Mines"
 
 
-    // add "Hell" to portalLocations after "Zathar House" and before "Osteroc"
+// add "Hell" to portalLocations after "Zathar House" and before "Osteroc"
 
 
-    // Change "Hell" to "Ghost Lord's Keep"
+// Change "Hell" to "Ghost Lord's Keep"
 
 
-    // remove "Ghost Lord's Keep" from portalLocations
+// remove "Ghost Lord's Keep" from portalLocations
 
+// Array 2
 
+let
--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-// Object
+// Object 1
 let playerCharacters = {
     "Druid": "Akasha",
     "Rogue": "Elektropop",
@@ -10,26 +10,26 @@ console.log(playerCharacters)
 
 /* 1. Return data from an object */
 
-    // Return the value of "Warlock"
+// Return the value of "Warlock"
 
-    // BONUS : Return any key
+// BONUS : Return any key
 
 
 
 
 /* 2.  Add and remove key/value pairs */
 
-    // add the key/value pair, "Dragonborn" : "Za'a'a'a'a'thar"
+// add the key/value pair, "Dragonborn" : "Za'a'a'a'a'thar"
 
 
-    // Remove key/value pair
+// Remove key/value pair
 
 
 
 
 /* BONUS  */
 
-    // Refactor so the functions so they are reusable 
+// Refactor so the functions so they are reusable 
 
 
 
@@ -38,7 +38,41 @@ console.log(playerCharacters)
 /* 3. -------------------------------------------------
 Change value and key */
 
-    // Change value "Nico" to "Nicolas"
+// Change value "Nico" to "Nicolas"
 
 
-    // Change key "Bard" to "BestClass"
+// Change key "Bard" to "BestClass"
+
+let efreetiBottle = {
+    "index": "efreeti-bottle",
+    "name": "Efreeti Bottle",
+    "equipment_category": {
+        "index": "wondrous-items",
+        "name": "Wondrous Items",
+        "url": "/api/equipment-categories/wondrous-items"
+    },
+    "desc": [
+        "Wondrous item, very rare",
+        "This painted brass bottle weighs 1 pound. When you use an action to remove the stopper, a cloud of thick smoke flows out of the bottle. At the end of your turn, the smoke disappears with a flash of harmless fire, and an efreeti appears in an unoccupied space within 30 feet of you.",
+        "The first time the bottle is opened, the GM rolls to determine what happens.",
+        "| d100 | Effect |",
+        "|---|---|",
+        "| 01-10 | The efreeti attacks you. After fighting for 5 rounds, the efreeti disappears, and the bottle loses its magic. |",
+        "| 11-90 | The efreeti serves you for 1 hour, doing as you command. Then the efreeti returns to the bottle, and a new stopper contains it. The stopper can't be removed for 24 hours. The next two times the bottle is opened, the same effect occurs. If the bottle is opened a fourth time, the efreeti escapes and disappears, and the bottle loses its magic. |",
+        "| 91-100 | The efreeti can cast the wish spell three times for you. It disappears when it grants the final wish or after 1 hour, and the bottle loses its magic. |"
+    ],
+    "url": "/api/magic-items/efreeti-bottle"
+};
+
+    // Print the value of the "name" key.
+
+    // Print the second element of "desc"
+
+    // Add a key value pair to efreetiBottle. The key should be called "Owner" and the value should be an array of strings that includes at least one element. 
+    // ex "owner": ["Samuel", "Rabbit"]
+
+
+    // Add another element to the "desc" array that adds to the description. 
+
+
+    // Loop over the description and print each line:
